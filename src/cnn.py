d5eb7c6fdef052f7c6dfec14ede0d3e47dfb02f3
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-21 20:37:19 +0000

    Add reading data and cnn for pneumonia

---
+++ src/cnn.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import pathlib
+from tensorflow.keras import layers
+
+
+TRAINDIR = "../data/chest_xray/train"
+TESTDIR = "../data/chest_xray/test"
+BATCHSIZE = 32
+IMG_HEIGHT = 200
+IMG_WIDTH = 200
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    TRAINDIR,
+    labels="inferred",
+    seed=1337,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCHSIZE,
+    color_mode="grayscale",
+)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    TESTDIR,
+    labels="inferred",
+    seed=1337,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCHSIZE,
+    color_mode="grayscale",
+)
+
+normalization_layer = layers.Rescaling(1.0 / 255)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Rescaling(1.0 / 255),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(1),
+    ]
+)
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+
+model.fit(train_ds, validation_data=test_ds, epochs=6)
