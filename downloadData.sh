9d0ca720d6a386f0a10382c0c7b58d5a8e5cbe09
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-22 11:49:33 +0100

    Add download data shell script

---
+++ downloadData.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p data &&
+wget https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded --output-document data/ChestXRay2017.zip &&
+unzip data/ChestXRay2017.zip
