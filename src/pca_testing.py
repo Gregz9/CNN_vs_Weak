74477521576fe43e015e6da93b91c2993ad01257
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-22 19:43:49 +0000

    Add time comparison with convolutional network

--- src/pca_testing.py
+++ src/pca_testing.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from tensorflow.keras import layers
 from PIL import Image
+import time
 import matplotlib.pyplot as plt
 
 # from sklearn.decomposition import PCA
@@ -11,9 +12,8 @@ mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
+print(y_train[:10])
 
-x_train = tf.reshape(x_train, shape=[-1, 784])
-x_test = tf.reshape(x_test, shape=[-1, 784])
 
 # PCA implemented using tensors, to be able to run on gpu
 class PCA:
@@ -57,40 +57,8 @@ class PCA:
 
 
 n_components = 100
-pca = PCA(n_components)
-pca.fit(x_train)
-x_train_pca = pca.transform(x_train)
-x_test_pca = pca.transform(x_test)
 
-x_train_remade = pca.inverse_transform(x_train_pca)
-
-
-# ------- plotting pca ------------
-plt.subplot(421)
-plt.title("Actual instance, \n 784 features")
-plt.imshow(tf.reshape(x_train[0], (28, 28)))
-plt.subplot(422)
-plt.title(f"Constructed using only the \n {n_components} first principal components")
-plt.imshow(tf.reshape(x_train_remade[0], (28, 28)))
-
-plt.subplot(423)
-plt.imshow(tf.reshape(x_train[1], (28, 28)))
-plt.subplot(424)
-plt.imshow(tf.reshape(x_train_remade[1], (28, 28)))
-
-plt.subplot(425)
-plt.imshow(tf.reshape(x_train[2], (28, 28)))
-plt.subplot(426)
-plt.imshow(tf.reshape(x_train_remade[2], (28, 28)))
-
-plt.subplot(427)
-plt.imshow(tf.reshape(x_train[3], (28, 28)))
-plt.subplot(428)
-plt.imshow(tf.reshape(x_train_remade[3], (28, 28)))
-
-plt.show()
-
-model = tf.keras.Sequential(
+flatmodel = tf.keras.Sequential(
     [
         # layers.Rescaling(1.0 / 255),
         # tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -102,15 +70,81 @@ model = tf.keras.Sequential(
     ]
 )
 
-model.compile(
+convmodel = tf.keras.Sequential(
+    [
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(10),
+    ]
+)
+
+flatmodel.compile(
     optimizer="adam",
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
 
-model.fit(x_train_pca, y_train, validation_data=(x_test_pca, y_test), epochs=6)
-model.summary()
+convmodel.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
 
+x_train_flat = tf.reshape(x_train, shape=[-1, 784])
+x_test_flat = tf.reshape(x_test, shape=[-1, 784])
+
+start = time.time()
+pca = PCA(n_components)
+pca.fit(x_train_flat)
+x_train_pca = pca.transform(x_train_flat)
+x_test_pca = pca.transform(x_test_flat)
+
+flatmodel.fit(
+    x_train_pca, y_train, validation_data=(x_test_pca, y_test), epochs=6, batch_size=64
+)
+print(f"PCA model time taken: {time.time() - start}")
+
+x_train_4d = tf.reshape(x_train, shape=[-1, 28, 28, 1])
+x_test_4d = tf.reshape(x_test, shape=[-1, 28, 28, 1])
+
+start = time.time()
+convmodel.fit(
+    x_train_4d, y_train, validation_data=(x_test_4d, y_test), epochs=6, batch_size=64
+)
+print(f"Conv model time taken: {time.time() - start}")
+
+# ------- plotting pca ------------
+x_train_remade = pca.inverse_transform(x_train_pca)
+
+plt.subplot(421)
+plt.title("Actual instance, \n 784 features")
+plt.imshow(x_train[0])
+plt.subplot(422)
+plt.title(f"Constructed using only the \n {n_components} first principal components")
+plt.imshow(tf.reshape(x_train_remade[0], (28, 28)))
+
+plt.subplot(423)
+plt.imshow(x_train[1])
+plt.subplot(424)
+plt.imshow(tf.reshape(x_train_remade[1], (28, 28)))
+
+plt.subplot(425)
+plt.imshow(x_train[2])
+plt.subplot(426)
+plt.imshow(tf.reshape(x_train_remade[2], (28, 28)))
+
+plt.subplot(427)
+plt.imshow(x_train[3])
+plt.subplot(428)
+plt.imshow(tf.reshape(x_train_remade[3], (28, 28)))
+
+plt.show()
 
 # class PCALayer(layers.Layer):
 #     def __init__(self, num_outputs):
