a545ddba0d3f2e2b6278750c432f4ca6acb0a339
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-23 10:27:14 +0100

    Mazunki "improvements"

--- downloadData.sh
+++ downloadData.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -e
+URL="https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded"
 
-mkdir -p data &&
-wget https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded --output-document data/ChestXRay2017.zip &&
+mkdir -p data
+wget "${URL}" --output-document data/ChestXRay2017.zip
 unzip data/ChestXRay2017.zip -d data
