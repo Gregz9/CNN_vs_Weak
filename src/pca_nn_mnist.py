4a5823573e2ba4374cf0a052cf6af2c7cc84037e
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-25 15:00:18 +0100

    Remove old code

--- src/pca_nn_mnist.py
+++ src/pca_nn_mnist.py
@@ -19,6 +19,8 @@ x_test_flat = tf.reshape(x_test, shape=[-1, 784])
 
 W = PCA_fit(x_train_flat, n_components)
 
+# train_ds.map(lambda batch, label: (tf.matmul(batch, W), label))
+
 model = tf.keras.Sequential(
     [
         PCALayer(W),
@@ -42,59 +44,3 @@ model.fit(
     epochs=6,
     batch_size=64,
 )
-
-# ------- plotting pca ------------
-# x_train_remade = pca.inverse_transform(x_train_pca)
-#
-# plt.subplot(421)
-# plt.title("Actual instance, \n 784 features")
-# plt.imshow(x_train[0])
-# plt.subplot(422)
-# plt.title(f"Constructed using only the \n {n_components} first principal components")
-# plt.imshow(tf.reshape(x_train_remade[0], (28, 28)))
-#
-# plt.subplot(423)
-# plt.imshow(x_train[1])
-# plt.subplot(424)
-# plt.imshow(tf.reshape(x_train_remade[1], (28, 28)))
-#
-# plt.subplot(425)
-# plt.imshow(x_train[2])
-# plt.subplot(426)
-# plt.imshow(tf.reshape(x_train_remade[2], (28, 28)))
-#
-# plt.subplot(427)
-# plt.imshow(x_train[3])
-# plt.subplot(428)
-# plt.imshow(tf.reshape(x_train_remade[3], (28, 28)))
-#
-# plt.show()
-
-# class PCALayer(layers.Layer):
-#     def __init__(self, num_outputs):
-#         super(PCALayer, self).__init__()
-#         self.num_outputs = num_outputs
-#         self.trainable = False
-#
-#     def call(self, inputs):
-#         batch_size = inputs.shape[0] or 1
-#         features = inputs.shape[1] * inputs.shape[2]
-#
-#         x_list = []
-#         for i in range(batch_size):
-#             x_list.append(tf.reshape(inputs[i], (features,)))
-#
-#         X = tf.stack(x_list)
-#
-#         means = tf.reduce_mean(X)
-#         print(means)
-#
-#         S, U, V = tf.linalg.svd(X)
-#
-#         slice_index = min(self.num_outputs, batch_size)
-#         S = S[:slice_index]
-#         S = tf.linalg.diag(S)
-#         U = U[:, :slice_index]
-#         output = tf.linalg.matmul(U, S)
-#
-#         return output
