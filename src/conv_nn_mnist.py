6821a712e7bbe9182f57a5ae0fc91a1bf5579b74
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-28 21:37:26 +0000

    Add hyperparameter tuning and crossvalidation

--- src/conv_nn_mnist.py
+++ src/conv_nn_mnist.py
@@ -6,6 +6,8 @@ from PIL import Image
 import time
 import matplotlib.pyplot as plt
 from utils import *
+from sklearn.model_selection import KFold
+import keras_tuner as kt
 
 batch_size = 128
 epochs = 6
@@ -16,38 +18,105 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.reshape(x_train, shape=[-1, 28, 28, 1])
 x_test = tf.reshape(x_test, shape=[-1, 28, 28, 1])
 
-print(x_train.shape)
+# print(x_train.shape)
+#
+# train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+# val_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+# print(train_ds)
+#
+# train_ds = train_ds.batch(batch_size)
+# val_ds = val_ds.batch(batch_size)
 
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-val_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-print(train_ds)
 
-train_ds = train_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
+def model_builder(hp):
+    hp_size = hp.Int("units", min_value=3, max_value=7, step=2)
 
-model = tf.keras.Sequential(
-    [
-        layers.Rescaling(1.0 / 255),
-        layers.Conv2D(32, 3, activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation="relu"),
-        layers.Dense(10),
-    ]
+    model = tf.keras.Sequential(
+        [
+            layers.Rescaling(1.0 / 255),
+            layers.Conv2D(32, hp_size, activation="relu", padding="same"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, hp_size, activation="relu", padding="same"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, hp_size, activation="relu", padding="same"),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(10),
+        ]
+    )
+
+    hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+tuner = kt.Hyperband(
+    model_builder,
+    objective="val_accuracy",
+    max_epochs=10,
+    factor=3,
+    directory="conv_nn_mnist-params",
 )
 
-model.compile(
-    optimizer="adam",
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
+print("searching")
+tuner.search(x_train, y_train, epochs=10, validation_split=0.2)
+print("done")
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+print(best_hps.get("units"))
+print(best_hps.get("learning_rate"))
 
-model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs,
-)
+kf = KFold(n_splits=5)  # random_state=1336)
+
+
+avg_accuracy = 0
+for train_index, test_index in kf.split(x_train, y_train):
+    train_start = train_index[0]
+    train_stop = train_index[-1]
+
+    test_start = test_index[0]
+    test_stop = test_index[-1]
+
+    model = tf.keras.Sequential(
+        [
+            layers.Rescaling(1.0 / 255),
+            layers.Conv2D(32, 3, activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(10),
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    model.fit(
+        x_train[train_start:train_stop],
+        y_train[train_start:train_stop],
+        validation_data=(x_train[test_start:test_stop], y_train[test_start:test_stop]),
+        epochs=epochs,
+    )
+
+    results = model.evaluate(
+        x_train[test_start:test_stop],
+        y_train[test_start:test_stop],
+        return_dict=True,
+    )
+    print(results)
+    avg_accuracy += results["accuracy"] / 5
+
+print(avg_accuracy)
--- src/pca_nn_mnist.py
+++ src/pca_nn_mnist.py
@@ -17,37 +17,6 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 n_components = 20
 
-
-def model_builder(hp):
-    hp_units = hp.Int("units", min_value=32, max_value=512, step=32)
-    model = tf.keras.Sequential(
-        [
-            layers.Dense(units=hp_units, activation="relu"),
-            layers.Dense(units=hp_units, activation="relu"),
-            layers.Dense(units=hp_units, activation="relu"),
-            layers.Dense(10),
-        ]
-    )
-
-    hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
-
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
-    )
-
-    return model
-
-
-tuner = kt.Hyperband(
-    model_builder,
-    objective="val_accuracy",
-    max_epochs=10,
-    factor=3,
-)
-
-
 x_train_flat = tf.reshape(x_train, shape=[-1, 784])
 x_test_flat = tf.reshape(x_test, shape=[-1, 784])
 
@@ -61,11 +30,54 @@ pca.fit(x_train_flat)
 pca.transform(x_train_flat)
 pca.transform(x_test_flat)
 
-tuner.search(x_train_flat, y_train, epochs=10, validation_split=0.2)
-best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+# def model_builder(hp):
+#     hp_units = hp.Int("units", min_value=32, max_value=512, step=32)
+#     model = tf.keras.Sequential(
+#         [
+#             layers.Dense(units=hp_units, activation="relu"),
+#             layers.Dense(units=hp_units, activation="relu"),
+#             layers.Dense(units=hp_units, activation="relu"),
+#             layers.Dense(10),
+#         ]
+#     )
+#
+#     hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+#
+#     model.compile(
+#         optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
+#         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#         metrics=["accuracy"],
+#     )
+#
+#     return model
+#
+#
+# tuner = kt.Hyperband(
+#     model_builder,
+#     objective="val_accuracy",
+#     max_epochs=10,
+#     factor=3,
+# )
+#
+# tuner.search(x_train_flat, y_train, epochs=10, validation_split=0.2)
+# best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+# print(best_hps.get("units"))
+# print(best_hps.get("learning_rate"))
 
-print(best_hps.get("units"))
-print(best_hps.get("learning_rate"))
+model = tf.keras.Sequential(
+    [
+        layers.Dense(224, activation="relu"),
+        layers.Dense(224, activation="relu"),
+        layers.Dense(224, activation="relu"),
+        layers.Dense(10),
+    ]
+)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
 
 model.fit(
     x_train_flat,
