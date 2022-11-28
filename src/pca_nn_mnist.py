0d4cd295d9066da7c78cbbf005e9d50c59a1124b
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-28 16:51:18 +0100

    Add hyperparameter tuning

--- src/pca_nn_mnist.py
+++ src/pca_nn_mnist.py
@@ -7,6 +7,7 @@ import time
 import matplotlib.pyplot as plt
 from utils import *
 from sklearn.decomposition import PCA
+import keras_tuner as kt
 
 mnist = tf.keras.datasets.mnist
 
@@ -14,6 +15,37 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 n_components = 100
+
+
+def model_builder(hp):
+    hp_units = hp.Int("units", min_value=32, max_value=512, step=32)
+    model = tf.keras.Sequential(
+        [
+            layers.Dense(units=hp_units, activation="relu"),
+            layers.Dense(units=hp_units, activation="relu"),
+            layers.Dense(units=hp_units, activation="relu"),
+            layers.Dense(10),
+        ]
+    )
+
+    hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+tuner = kt.Hyperband(
+    model_builder,
+    objective="val_accuracy",
+    max_epochs=10,
+    factor=3,
+)
+
 
 x_train_flat = tf.reshape(x_train, shape=[-1, 784])
 x_test_flat = tf.reshape(x_test, shape=[-1, 784])
@@ -27,21 +59,11 @@ pca.fit(x_train_flat)
 pca.transform(x_train_flat)
 pca.transform(x_test_flat)
 
+tuner.search(x_train_flat, y_train, epochs=10, validation_split=0.2)
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
-model = tf.keras.Sequential(
-    [
-        layers.Dense(n_components, activation="relu"),
-        layers.Dense(n_components, activation="relu"),
-        layers.Dense(n_components, activation="relu"),
-        layers.Dense(10),
-    ]
-)
-
-model.compile(
-    optimizer="adam",
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
+print(best_hps.get("units"))
+print(best_hps.get("learning_rate"))
 
 model.fit(
     x_train_flat,
