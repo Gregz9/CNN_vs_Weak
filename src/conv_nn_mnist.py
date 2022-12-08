de4b9e0058a223ab3f39b5bb746df56df64cdc6e
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-12-08 16:18:54 +0100

    Update this file to be about finished

--- src/conv_nn_mnist.py
+++ src/conv_nn_mnist.py
@@ -12,11 +12,13 @@ import seaborn as sns
 
 tf.keras.utils.set_random_seed(1336)
 """
-Convolutional neural network used for MNIST dataset. Builds and fits data, takes time.
+Convolutional neural network used for MNIST dataset. Performs hyperparameter tuning,
+trains the model, prints average time for making a prediction on the entire test set
+creates a confusion matrix
 """
 
 batch_size = 128
-epochs = 6
+epochs = 1
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -24,26 +26,14 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.reshape(x_train, shape=[-1, 28, 28, 1])
 x_test = tf.reshape(x_test, shape=[-1, 28, 28, 1])
 
-# print(x_train.shape)
-#
-# train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-# val_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-# print(train_ds)
-#
-# train_ds = train_ds.batch(batch_size)
-# val_ds = val_ds.batch(batch_size)
 
-
+# Hyperparameter tuning model
 def model_builder(hp):
     hp_width = hp.Choice("width", values=[3, 5, 7])
 
     model = tf.keras.Sequential(
         [
             layers.Rescaling(1.0 / 255),
-            # layers.Conv2D(32, hp_width, activation="relu", padding="same"),
-            # layers.MaxPooling2D(),
-            # layers.Conv2D(32, hp_width, activation="relu", padding="same"),
-            # layers.MaxPooling2D(),
             layers.Conv2D(32, hp_width, activation="relu", padding="same"),
             layers.MaxPooling2D(),
             layers.Flatten(),
@@ -71,22 +61,18 @@ tuner = kt.Hyperband(
     directory="conv_nn_mnist-params",
 )
 
+# finding the best hyperparameters
 tuner.search(x_train, y_train, epochs=10, validation_split=0.2)
 best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 width = best_hps.get("width")
 learning_rate = best_hps.get("learning_rate")
-print(width)
-print(learning_rate)
+print(f"Best kernel width found by tuner: {width}")
+print(f"Best learning rate found by tuner: {learning_rate}")
 
-
-start = time.time()
+# using the best hyperparameters
 model = tf.keras.Sequential(
     [
         layers.Rescaling(1.0 / 255),
-        # layers.Conv2D(32, width, activation="relu", padding='same'),
-        # layers.MaxPooling2D(),
-        # layers.Conv2D(32, width, activation="relu", padding='same'),
-        # layers.MaxPooling2D(),
         layers.Conv2D(32, width, activation="relu", padding="same"),
         layers.MaxPooling2D(),
         layers.Flatten(),
@@ -110,6 +96,7 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     save_best_only=True,
 )
 
+# fitting the model
 model.fit(
     x_train,
     y_train,
@@ -119,15 +106,11 @@ model.fit(
     callbacks=[model_checkpoint_callback],
 )
 
+# loading the best weights from the model
 model.load_weights(checkpoint_filepath)
 
-print(f"Time taken: {time.time() - start}")
-
-results = model.evaluate(
-    x_test,
-    y_test,
-    return_dict=True,
-)
+print("Timing model:")
+timeit(model.evaluate, x_test, y_test)
 
 predictions = tf.math.argmax(model.predict(x_test), axis=1)
 conf = conf_mat(predictions, y_test, num_cls=10)
