710c46c4e7af546c4cc1733749bf0bda5e4a760d
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-12-08 12:50:16 +0100

    Add pca rf pneu new version

--- src/pca_rf_pneu.py
+++ src/pca_rf_pneu.py
@@ -2,75 +2,146 @@ import tensorflow as tf
 import os
 import numpy as np
 from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+import keras_tuner as kt
 from PIL import Image
 import time
 import matplotlib.pyplot as plt
 from utils import *
-import tensorflow_decision_forests as tfds
+from sklearn.decomposition import PCA
 
 filedir = os.path.dirname(__file__)
 
 tf.keras.utils.set_random_seed(1336)
 """
-PCA random forest used for pneumonia dataset. Builds and fits data.
+PCA neural network used for pneumonia dataset. Builds and fits data, takes time.
 """
 
-TRAINDIR = filedir + "/../data/chest_xray/train"
-TESTDIR = filedir + "/../data/chest_xray/test"
-BATCHSIZE = 128
-IMG_HEIGHT = 200
-IMG_WIDTH = 200
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    TRAINDIR,
-    labels="inferred",
-    seed=1337,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=128,
-    color_mode="grayscale",
-)
+def model_builder(hp):
+    hp_lambda = hp.Choice("lambda", values=[1e-5, 1e-4, 1e-3])
+    hp_learning_rate = hp.Choice("learning_rate", values=[1e-3, 1e-2, 1e-1])
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    TESTDIR,
-    labels="inferred",
-    seed=1337,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=128,
-    color_mode="grayscale",
-)
+    kernel = tf.keras.regularizers.L2(l2=hp_lambda)
+    bias = tf.keras.regularizers.L2(l2=hp_lambda)
 
+    model = tf.keras.Sequential(
+        [
+            layers.Dense(
+                n_components,
+                kernel_regularizer=kernel,
+                bias_regularizer=bias,
+                activation="relu",
+            ),
+            layers.Dense(
+                n_components,
+                kernel_regularizer=kernel,
+                bias_regularizer=bias,
+                activation="relu",
+            ),
+            layers.Dense(
+                n_components,
+                kernel_regularizer=kernel,
+                bias_regularizer=bias,
+                activation="relu",
+            ),
+            layers.Dense(1),
+        ]
+    )
 
-# grab a subset for PCA calculation
-x_list = []
-i = 0
-for batch, _ in train_ds:
-    x_list.append(tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0)
-    i += 1
-    if i > 20:
-        break
+    model.compile(
+        optimizer=tf.keras.optimizers.Adamax(learning_rate=hp_learning_rate),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
 
-X_subset = tf.concat(x_list, axis=0)
-n_components = 2000
-
-W = PCA_fit(X_subset, n_components)
-
-print(train_ds)
-train_ds = train_ds.map(
-    lambda batch, label: (tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0, label)
-)
-val_ds = val_ds.map(
-    lambda batch, label: (tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0, label)
-)
-print(train_ds)
-train_ds = train_ds.map(lambda batch, label: (tf.matmul(batch, W), label))
-val_ds = val_ds.map(lambda batch, label: (tf.matmul(batch, W), label))
-print(train_ds)
-print(val_ds)
+    return model
 
 
-model = tfds.keras.RandomForestModel(verbose=1, max_depth=10, check_dataset=False)
+with tf.device("/cpu:0"):
+    TRAINDIR = filedir + "/../data/chest_xray/train"
+    TESTDIR = filedir + "/../data/chest_xray/test"
+    BATCHSIZE = 128
+    IMG_HEIGHT = 200
+    IMG_WIDTH = 200
 
-model.fit(x=train_ds)
-model.compile(metrics=["accuracy"])
-print(model.evaluate(train_ds, return_dict=True))
-print(model.evaluate(val_ds, return_dict=True))
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        TRAINDIR,
+        labels="inferred",
+        seed=1337,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCHSIZE,
+        color_mode="grayscale",
+    )
+
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+        TESTDIR,
+        labels="inferred",
+        seed=1337,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCHSIZE,
+        color_mode="grayscale",
+    )
+
+    COUNT_NORMAL = 1071
+    COUNT_PNEUMONIA = 3114
+
+    weight_for_0 = (1 / COUNT_NORMAL) * (COUNT_NORMAL + COUNT_PNEUMONIA) / 2.0
+    weight_for_1 = (1 / COUNT_PNEUMONIA) * (COUNT_NORMAL + COUNT_PNEUMONIA) / 2.0
+
+    class_weight = {0: weight_for_0, 1: weight_for_1}
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    x_list = []
+    i = 0
+    X_train = None
+    X_test = None
+    y_train = None
+    y_test = None
+
+    n_components = 20
+
+    for batch, labels in train_ds:
+        if X_train is None:
+            X_train = tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0
+            y_train = labels
+
+        else:
+            X_train = tf.concat(
+                [X_train, (tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0)], axis=0
+            )
+            y_train = tf.concat([y_train, labels], axis=0)
+
+    for batch, labels in test_ds:
+        if X_test is None:
+            X_test = tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0
+            y_test = labels
+
+        else:
+            X_test = tf.concat(
+                [X_test, (tf.reshape(batch, shape=[-1, 200 * 200]) / 255.0)], axis=0
+            )
+            y_test = tf.concat([y_test, labels], axis=0)
+
+    pca = PCA(n_components=n_components, svd_solver="randomized", random_state=1336)
+
+    print("Fitting PCA")
+    start = time.time()
+    pca.fit(X_train)
+
+    pca.transform(X_train)
+    pca.transform(X_test)
+
+    forest = tfdf.keras.RandomForestModel(
+        verbose=1, max_depth=16, random_seed=1337, check_dataset=False
+    )
+    forest.fit(X_train, y_train)
+
+    forest.compile(metrics=["accuracy"])
+
+    print(forest.evaluate(X_train, y_train, return_dict=True))
+    print(forest.evaluate(X_test, y_test, return_dict=True))
